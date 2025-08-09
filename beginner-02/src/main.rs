604814316850eed389274b366fb4f9f8ae3f0041
俺不叫化咕龙 <1801943622@qq.com>
2025-08-09 19:21:47 +0800

    Add utils dependency and implement window handling in WgpuApp; enhance framework for resizing and rendering

--- beginner-02/src/main.rs
+++ beginner-02/src/main.rs
@@ -1,8 +1,10 @@
-use app_surface::AppSurface;
+use app_surface::{AppSurface, SurfaceFrame};
+use utils::framework::run;
 use winit::dpi::PhysicalSize;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    run::<WgpuApp>("tutorial3-pipeline")?;
+    Ok(())
 }
 
 struct WgpuApp {
@@ -10,4 +12,142 @@ struct WgpuApp {
     size: PhysicalSize<u32>,
     size_changed: bool,
     render_pipeline: wgpu::RenderPipeline,
+}
+
+impl WgpuApp {
+    fn resize(&mut self) {
+        if self.size_changed {
+            self.app
+                .resize_surface_by_size((self.size.width, self.size.height));
+            self.size_changed = false;
+        }
+    }
+}
+
+impl utils::framework::WgpuAppAction for WgpuApp {
+    async fn new(window: std::sync::Arc<winit::window::Window>) -> Self {
+        let app = AppSurface::new(window).await;
+
+        let shader = app
+            .device
+            .create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("Shader"),
+                source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+            });
+
+        let render_pipeline_layout =
+            app.device
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[],
+                    push_constant_ranges: &[],
+                });
+
+        let render_pipeline = app
+            .device
+            .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("render pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: Some("vs_main"),
+                    compilation_options: Default::default(),
+                    buffers: &[],
+                },
+
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    unclipped_depth: false,
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: Some("fs_main"),
+                    compilation_options: Default::default(),
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: app.config.format.add_srgb_suffix(),
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                multiview: None,
+                cache: None,
+            });
+
+        let size = PhysicalSize {
+            width: app.config.width,
+            height: app.config.height,
+        };
+
+        Self {
+            app,
+            size,
+            size_changed: false,
+            render_pipeline,
+        }
+    }
+
+    fn set_window_size(&mut self, new_size: PhysicalSize<u32>) {
+        if self.app.config.width == new_size.width && self.app.config.height == new_size.height {
+            return;
+        }
+
+        self.size = new_size;
+        self.size_changed = true;
+    }
+
+    fn get_size(&self) -> PhysicalSize<u32> {
+        PhysicalSize::new(self.app.config.width, self.app.config.height)
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        self.resize();
+
+        let (output, view) = self.app.get_current_frame_view(None);
+        let mut encoder = self
+            .app
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    depth_slice: None,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.4,
+                            g: 0.1,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                ..Default::default()
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
+
+        self.app.queue.submit(Some(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
 }
--- utils/src/framework.rs
+++ utils/src/framework.rs
@@ -1,11 +1,19 @@
 use std::sync::Arc;
 
+use parking_lot::Mutex;
 use wgpu::WasmNotSend;
 use winit::{
+    application::ApplicationHandler,
     dpi::{PhysicalPosition, PhysicalSize},
-    event::{DeviceEvent, ElementState, KeyEvent, MouseButton, MouseScrollDelta, TouchPhase},
+    event::{
+        DeviceEvent, ElementState, KeyEvent, MouseButton, MouseScrollDelta, TouchPhase, WindowEvent,
+    },
+    event_loop::EventLoop,
     window::Window,
 };
+
+#[cfg(target_arch = "wasm32")]
+use winit::platform::web::WindowExtWebSys;
 
 pub trait WgpuAppAction {
     fn new(window: Arc<Window>) -> impl core::future::Future<Output = Self> + WasmNotSend;
@@ -35,4 +43,162 @@ pub trait WgpuAppAction {
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError>;
+}
+
+struct WgpuAppHandler<A> {
+    window: Option<Arc<Window>>,
+    title: &'static str,
+    app: Arc<Mutex<Option<A>>>,
+    #[allow(dead_code)]
+    missed_resize: Arc<Mutex<Option<PhysicalSize<u32>>>>,
+}
+
+impl<A> WgpuAppHandler<A> {
+    fn new(title: &'static str) -> Self {
+        Self {
+            window: None,
+            title,
+            app: Arc::new(Mutex::new(None)),
+            missed_resize: Arc::new(Mutex::new(None)),
+        }
+    }
+
+    fn config_window(&mut self) {
+        let window = self.window.as_mut().unwrap();
+        window.set_title(self.title);
+        if cfg!(not(target_arch = "wasm32")) {
+            // 计算一个默认显示高度
+            let height = 600 * window.scale_factor() as u32;
+            let width = height;
+            let _ = window.request_inner_size(PhysicalSize::new(width, height));
+        }
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            let canvas = window.canvas().unwrap();
+
+            // 将 canvas 添加到当前网页中
+            web_sys::window()
+                .and_then(|win| win.document())
+                .map(|doc| {
+                    let _ = canvas.set_attribute("id", "winit-canvas");
+                    match doc.get_element_by_id("wgpu-app-container") {
+                        Some(dst) => {
+                            let _ = dst.append_child(canvas.as_ref());
+                        }
+                        None => {
+                            let container = doc.create_element("div").unwrap();
+                            let _ = container.set_attribute("id", "wgpu-app-container");
+                            let _ = container.append_child(canvas.as_ref());
+
+                            doc.body().map(|body| body.append_child(container.as_ref()));
+                        }
+                    };
+                })
+                .expect("无法将 canvas 添加到当前网页中");
+
+            // 确保画布可以获得焦点
+            // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/tabindex
+            canvas.set_tab_index(0);
+
+            // 设置画布获得焦点时不显示高亮轮廓
+            let style = canvas.style();
+            style.set_property("outline", "none").unwrap();
+            canvas.focus().expect("画布无法获取焦点");
+        }
+    }
+}
+
+impl<A: WgpuAppAction> ApplicationHandler for WgpuAppHandler<A> {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        if self.app.as_ref().lock().is_some() {
+            return;
+        }
+
+        let window_attributes = Window::default_attributes();
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+
+        self.window = Some(window.clone());
+        self.config_window();
+
+        cfg_if::cfg_if! {
+            if #[cfg(target_arch = "wasm32")] {
+                let app = self.app.clone();
+                let missed_resize = self.missed_resize.clone();
+
+                wasm_bindgen_futures::spawn_local(async move {
+                    let window_cloned = window.clone();
+
+                    let wgpu_app = A::new(window).await;
+                    let mut app = app.lock();
+                    *app = Some(wgpu_app);
+
+                    if let Some(resize) = *missed_resize.lock() {
+                        app.as_mut().unwrap().set_window_resized(resize);
+                        window_cloned.request_redraw();
+                    }
+                });
+            } else {
+                let wgpu_app = pollster::block_on(A::new(window));
+                self.app.lock().replace(wgpu_app);
+            }
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        let mut app = self.app.lock();
+        if app.as_ref().is_none() {
+            if let WindowEvent::Resized(physical_size) = event
+                && physical_size.width > 0
+                && physical_size.height > 0
+            {
+                let mut missed_resize = self.missed_resize.lock();
+                *missed_resize = Some(physical_size);
+            }
+            return;
+        }
+
+        let app = app.as_mut().unwrap();
+        match event {
+            WindowEvent::Resized(physical_size) => {
+                if physical_size.width == 0 || physical_size.height == 0 {
+                    log::info!("Window minimized!");
+                } else {
+                    log::info!("Window resized: {:?}", physical_size);
+
+                    app.set_window_size(physical_size);
+                }
+            }
+            WindowEvent::RedrawRequested => {
+                self.window
+                    .as_ref()
+                    .map(|window| window.pre_present_notify());
+
+                match app.render() {
+                    Ok(_) => {}
+                    Err(wgpu::SurfaceError::Lost) => eprintln!("Surface is lost"),
+                    Err(e) => eprintln!("{e:?}"),
+                }
+
+                self.window.as_ref().map(|window| window.request_redraw());
+            }
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            _ => {}
+        }
+    }
+}
+
+pub fn run<A: WgpuAppAction + 'static>(title: &'static str) -> Result<(), impl std::error::Error> {
+    crate::init_logger();
+
+    let events_loop = EventLoop::new().unwrap();
+    let mut app = WgpuAppHandler::<A>::new(title);
+    events_loop.run_app(&mut app)
 }
