e5bc4cd9c95242c2d889e9b4798cb778ca7c67af
俺不叫化咕龙 <1801943622@qq.com>
2025-08-11 17:46:49 +0800

    Refactor vertex data and implement index buffer in WgpuApp; enhance rendering logic for improved performance

--- beginner/src/main.rs
+++ beginner/src/main.rs
@@ -21,18 +21,28 @@ struct Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0, 1.0],
-    },
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5, 1.0],
+    }, // A
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0, 1.0],
-    },
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5, 1.0],
+    }, // B
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0, 1.0],
-    },
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5, 1.0],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5, 1.0],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5, 1.0],
+    }, // E
 ];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 struct WgpuApp {
     #[allow(unused)]
@@ -48,6 +58,7 @@ struct WgpuApp {
     use_color: bool,
     other_pipeline: wgpu::RenderPipeline,
     buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
 }
 
 impl WgpuApp {
@@ -173,6 +184,12 @@ impl WgpuApp {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
         let other_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
@@ -233,6 +250,7 @@ impl WgpuApp {
             other_pipeline,
             use_color: true,
             buffer,
+            index_buffer,
         }
     }
 
@@ -300,13 +318,16 @@ impl WgpuApp {
                 ..Default::default()
             });
 
-            render_pass.set_vertex_buffer(0, self.buffer.slice(..));
-            render_pass.set_pipeline(if self.use_color {
-                &self.render_pipeline
+            if self.use_color {
+                render_pass.set_pipeline(&self.render_pipeline);
+                render_pass.draw(0..3, 0..1);
             } else {
-                &self.other_pipeline
-            });
-            render_pass.draw(0..3, 0..1);
+                render_pass.set_pipeline(&self.other_pipeline);
+                render_pass.set_vertex_buffer(0, self.buffer.slice(..));
+                render_pass
+                    .set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+                render_pass.draw_indexed(0..INDICES.len() as u32, 0, 0..1);
+            }
         }
 
         self.queue.submit(Some(encoder.finish()));
